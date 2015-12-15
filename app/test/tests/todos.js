1c013af3d2de670fb9f68f16c2664053195b5802
Dmitry_Kabardinov <Dmitry_Kabardinov@epam.com>
2015-12-15 18:42:15 +0300

    add first test for todos module

--- app/test/tests/todos.js
+++ app/test/tests/todos.js
@@ -2,22 +2,26 @@
  * Created by dmitriy on 14.12.15.
  */
 define(
-    ['sinon'],
-    function (sinon) {
+    ['sinon', 'todos'],
+    function (sinon, Todos) {
         suite('todos module', function() {
             setup(function() {
 
             });
 
             suite('onCheck method', function() {
-                test('gets id of changed checkbox', function() {
-                    fake
-                    var fakeEvent = {
-                        target: {
 
-                        }
+                test('gets id of changed checkbox', function() {
+                    var containerBlock = document.createElement('div');
+                    document.body.appendChild(containerBlock);
+                    containerBlock.setAttribute('id', 'todos');
+                    var todos = new Todos('todos');
+                    var fakeCheckbox = document.createElement('input');
+                    fakeCheckbox.setAttribute('type', 'checkbox');
+                    var fakeEvent = {
+                        target: fakeCheckbox
                     };
-                    assert.equal(this.container.tagName, 'DIV');
+                    assert.isFunction(todos.onCheck, 'DIV');
                 });
             });
 
